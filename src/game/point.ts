fded3da70a9e8625cbd50d3fb997d9b6eb4922ad
lucas <stempest@163.com>
2023-04-27 18:07:38 +0800

    添加角度函数, 添加冲锋demo

--- src/game/point.ts
+++ src/game/point.ts
@@ -1,3 +1,5 @@
+import { toAngle } from "../utils/math";
+
 type Type2DAxis = { x: number; y: number };
 
 /**
@@ -11,6 +13,28 @@ function getDistance_2D(axis1: Type2DAxis, axis2: Type2DAxis) {
   return Math.sqrt(
     Math.pow(axis2.x - axis1.x, 2) + Math.pow(axis2.y - axis1.y, 2)
   );
+}
+
+/**
+ * 获取点1到点2的弧度/角度
+ * @param axis1 点1
+ * @param axis2 点2
+ * @param useAngle 使用角度
+ * @returns
+ */
+function getRadian_2D(
+  axis1: Type2DAxis,
+  axis2: Type2DAxis,
+  useAngle: boolean = false
+): number {
+  const dx = axis2.x - axis1.x,
+    dy = axis2.y - axis1.y;
+  const radian = Math.atan2(dy, dx);
+
+  if (useAngle) {
+    return toAngle(radian);
+  }
+  return radian;
 }
 
 /**
@@ -84,4 +108,34 @@ function isInPointRange(
 ) {
   const distance = getDistance_2D(referAxis, checkAxis);
   return distance <= r ? true : false;
+}
+
+/**
+ * 二维坐标系中，从一个点移动到另外一个点
+ * @param startAxis
+ * @param endAxis
+ * @param speed
+ * @ps 默认按照60/30FPS标准来决定渲染循环定时器间隔
+ * @remarks
+ *  1. 这里只是演示，没有代入更新。
+ *  2. 由于是预先计算，因此终点移动不会进行跟踪。
+ *  3. 暂未代入角度，需要结合三角函数。
+ *  4. 其实可以通过getAnyPoint，来获取到目标的每个点路径。
+ */
+function cf_demo(startAxis: Type2DAxis, endAxis: Type2DAxis, speed: number) {
+  const distance = getDistance_2D(startAxis, endAxis);
+  if (speed >= distance) {
+    return [endAxis];
+  } else {
+    const moveAxisData: Type2DAxis[] = [];
+    const count = Math.ceil(distance / speed);
+    for (let i = 1; i <= count; i++) {
+      //可以在这里根据角度/弧度使用三角函数计算坐标
+      moveAxisData.push({
+        x: startAxis.x + i * speed,
+        y: startAxis.y + i * speed,
+      });
+    }
+    return moveAxisData;
+  }
 }
